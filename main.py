6757a018b6b5d378b05ca0ea1554c4c9148a8d60
jelars77 <jameslarson077@gmail.com>
2024-08-23 17:00:17 -0700

    Created asteroidfield.py to continuosly spawn multiple asteroid objects and integrated into the main.py file

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()    
     print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids_group = pygame.sprite.Group()
-
+    asteroid_field = AsteroidField(updatable, drawable, asteroids_group)
+    updatable.add(asteroid_field)
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
-    asteroid = Asteroid(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, 20, (50, 50))
+    initial_velocity = pygame.Vector2(50, 50)
+    
+    # Create the asteroid with the initial velocity
+    asteroid = Asteroid(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, 20, initial_velocity)
+    
+    # Add the asteroid to the respective groups
     asteroids_group.add(asteroid)
     updatable.add(asteroid)
     drawable.add(asteroid)
